ab0d71e8abae85f82c6a732e177dfd5796b8b1f6
ericwpeterson <epet2910@gmail.com>
2017-06-15 15:41:17 -0500

    Create dog.js
    removed commented out code

--- server/src/monobjects/reducers/dog.js
+++ server/src/monobjects/reducers/dog.js
@@ -15,21 +15,9 @@ const DEFAULT_STATE = Map({
     })
 });
 
-//console.log(JSON.stringify(DEFAULT_STATE.toJS(), null, 4));
+export default function dogReducer(state = DEFAULT_STATE, action) {    
 
-//export default function(wrapped) {
-
-export default function dogReducer(state = DEFAULT_STATE, action) {
-
-    //let ret = function(state = DEFAULT_STATE, action) {
-
-    switch (action.type) {
-
-        //NOTE: just showing we could always handle this instead of the wrapped generic reducer
-        //case SET_PROP:
-        //    if ( action.monObject === 'ups') {
-        //        return setProperty(state, action);
-        //    }
+    switch (action.type) {      
 
         case CALL_METHOD:
             if (action.monObject === 'dog' && action.method === 'bark') {
@@ -38,15 +26,8 @@ export default function dogReducer(state = DEFAULT_STATE, action) {
 
         default:
             return state;
-    }
-    //return wrapped(state, action);
-    //};
-    //return ret;
+    }    
 }
-
-let setProperty = (state, action) => {
-    return state;
-};
 
 let bark = (state, action) => {
     let path = [action.monObject, 'methods', action.method];
